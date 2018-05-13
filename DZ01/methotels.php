c4360f82a1bc23b8337392ee02300ffd35e59773
yomla <djoml8bre@gmail.com>
2018-05-13 20:25:56 +0200

    DZ01 - Met Hotel projekat

---
+++ DZ01/methotels.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Met hotels</title>
+		<style>
+			html {
+				background-color: lightgray
+			}
+
+			body {
+				background-color: lightblue; 
+				margin: 0 auto; 
+				width: 800px;
+				height: 500px;
+				text-align: center
+			}
+		</style>
+	</head>
+	<body>
+		<h1>
+			<?php		
+				echo "MET HOTELS";
+			?>
+		</h1>
+		<p>
+			<?php
+				echo "Pokrenut projekat! :D";
+			?>
+		</p>
+	</body>
+</html>
